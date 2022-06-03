9daa0c6fcb07ad4aceb2ee2a38b27b48d26ba114
BluesM18A1 <36201999+CathodeRayBlues@users.noreply.github.com>
2022-06-03 05:18:20 -0700

    OpenCV implementation works now
    it's still super slow :(

--- src/main.rs
+++ src/main.rs
@@ -9,42 +9,40 @@ use opencv::imgcodecs;
 
 fn main(){
     println!("let's make some noise...");
-    //todo: automatically detect how many frames need decoding
-    let frame_count = 1;
+    //todo: CLI parameters for input and output files
     
-    let mut i = 1;
-    //initialize file
+    //initialize output file
     let mut base64out = File::create("/home/blues/test64out.txt").unwrap();
-
+    //load the source video
     let mut video_file = videoio::VideoCapture::from_file("/home/blues/frames/test.mp4", videoio::CAP_FFMPEG).unwrap();
     
+    //grab the first frame
     let mut frame = Mat::default();
-    let _frame_read_success = video_file.read(&mut frame).unwrap();
+    let mut frame_read_success = video_file.read(&mut frame).unwrap();
     
-    //let fakeImageBuffer : std::vec::Vec<u8> = Vec::new();
-    let mut imagebuffer : opencv::core::Vector<u8> = opencv::core::Vector::new();
-    let mut image_params : opencv::core::Vector<i32> = opencv::core::Vector::new();
-    let _image_encode_success = imgcodecs::imencode("png", &frame, &mut imagebuffer, &mut image_params);
-
-    while i <= frame_count{
-        //let directory = "/home/blues/frames/";
-        //let file_dir = format!("{}{}{}", directory, i, ".png");
+    while frame_read_success{
+        //make the image buffer
+        let mut imagebuffer : opencv::core::Vector<u8> = opencv::core::Vector::new();
+        let mut image_params : opencv::core::Vector<i32> = opencv::core::Vector::new();
+        let _image_encode_success = imgcodecs::imencode(".png", &frame, &mut imagebuffer, &mut image_params);
+        //convert opencv buffer type to standard vec
         let buffer = imagebuffer.to_vec();
-        let img = image::load_from_memory(&buffer).unwrap().to_luma8();
+        // get the image library to load the buffer and convert it to the format needed for QR grid detection
+        let img = image::load_from_memory_with_format(&buffer, image::ImageFormat::Png).unwrap().to_luma8();
         // Prepare for detection
         let mut img = rqrr::PreparedImage::prepare(img);
         // Search for grids, without decoding
         let grids = img.detect_grids();
-
-        println!("{}", grids.len());
-
+        //make sure a grid is found before continuing
         assert_eq!(grids.len(), 1);
         // Decode the grid
         let (_meta, content) = grids[0].decode().unwrap();
         //add decoded base64 line to buffer
+        print!("{}", content);
         write!(&mut base64out, "{}", content);
-        println!("decoded frame {} of {}", i, frame_count);
-        i += 1;
+        //grab the next frame, or exit loop
+        frame = Mat::default();
+        frame_read_success = video_file.read(&mut frame).unwrap();
     }
     //finish the file
     base64out.flush().unwrap();
