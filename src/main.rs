eb512029d17c42e3c21c37ff775f09ebed8d1c29
BluesM18A1 <36201999+CathodeRayBlues@users.noreply.github.com>
2022-06-03 10:06:37 -0700

    cleaning up unused dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use image;
 use zbar_rust::ZBarImageScanner;
 use std::fs::File;
 use std::io::Write;
@@ -25,13 +24,13 @@ fn main(){
     let mut frame_read_success = video_file.read(&mut frame).unwrap();
     let frame_count = video_file.get(videoio::CAP_PROP_FRAME_COUNT).unwrap();
     let mut current_frame = 1;
+
     while frame_read_success{
         //define width and height for this frame. Some videos change their resolution in the middle which is why I check this every frame instead of just once.
         let width = video_file.get(videoio::CAP_PROP_FRAME_WIDTH).unwrap() as u32;
         let height = video_file.get(videoio::CAP_PROP_FRAME_HEIGHT).unwrap() as u32;
-        //make the image buffer
-        let mut imagebuffer : opencv::core::Vector<u8> = opencv::core::Vector::new();
-        let mut image_params : opencv::core::Vector<i32> = opencv::core::Vector::new();
+        
+        //convert frame data to greyscale
         let mut gray = Mat::default();
 			opencv::imgproc::cvt_color(
 				&frame,
@@ -39,14 +38,17 @@ fn main(){
 				opencv::imgproc::COLOR_BGR2GRAY,
 				0,
 		).unwrap();
+
+        //make the image buffer
+        let mut imagebuffer : opencv::core::Vector<u8> = opencv::core::Vector::new();
+        let mut image_params : opencv::core::Vector<i32> = opencv::core::Vector::new();
         let _image_encode_success = imgcodecs::imencode(".pnm", &gray, &mut imagebuffer, &mut image_params);
         //convert opencv buffer type to standard vec
         let buffer = imagebuffer.to_vec();
-        // get the image library to load the buffer and convert it to the format needed for QR grid detection
-
+        //start scanning
         let mut scanner = ZBarImageScanner::new();
         let results = scanner.scan_y800(&buffer, width, height).unwrap();
-
+        //write results
         for result in results {
             let content = String::from_utf8(result.data).unwrap();
             println!("decoded frame {} of {}", current_frame,frame_count);
@@ -59,5 +61,4 @@ fn main(){
     }
     //finish the file
     base64out.flush().unwrap();
-    
 }
